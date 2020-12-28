aafe8ac6b87a37c77518c10647985fecc7bae607
Garygohjr <garygohjr@gmail.com>
2020-12-28 16:55:02 +0800

    First Commit

---
+++ Task1/Task1/Task1CSharp.aspx
@@ -0,0 +1,2 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Task1CSharp.aspx.cs" Inherits="Task1.Task1CSharp" %>
+
---
+++ Task1/Task1/Task1CSharp.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace Task1
+{
+    public partial class Task1CSharp : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UriBuilder url = new UriBuilder();
+            url.Scheme = "https";
+
+            url.Host = "api.data.gov.sg";
+            url.Path = "v1/environment/2-hour-weather-forecast";
+
+            HttpWebRequest request = WebRequest.Create(url.ToString()) as HttpWebRequest;
+            request.Timeout = 15 * 1000;
+            request.KeepAlive = false;
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string content = reader.ReadToEnd();
+
+            Response.Write(content);
+
+        }
+
+    }
+}
