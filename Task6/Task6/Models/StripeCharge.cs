e7fb60067048acd5861657ae6ee3d093764d3434
garygohjr <garygohjr@gmail.com>
2021-01-04 12:45:01 +0800

    Task6

---
+++ Task6/Task6/Models/StripeCharge.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task6.Models
+{
+    public class StripeCharge
+    {
+        [Required]
+        public string Token { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string CardHolderName { get; set; }
+        [Required]
+        public string AddressLine1 { get; set; }
+        [Required]
+        public string AddressLine2 { get; set; }
+        [Required]
+        public string AddressCity { get; set; }
+        [Required]
+        [RegularExpression(@"^(\d{6})$", ErrorMessage = "Enter a valid 6 digit postalcode")]
+        public string AddressPostalcode { get; set; }
+        [Required]
+        public string AddressCountry { get; set; }
+    }
+}
