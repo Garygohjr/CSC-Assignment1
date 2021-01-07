b5647780225b4a1bf003f118a7d0c4e20700ddb6
garygohjr <garygohjr@gmail.com>
2021-01-07 12:10:29 +0800

    Removed Task3

--- Task3/LocalAccounts/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace LocalAccounts.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
